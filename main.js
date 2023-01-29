77ae1d389916b92616d29b1efedee324a4ddec80
bildukas86 <ekoncevicius@gmail.com>
2023-01-29 22:12:09 +0000

    add
    storing img links

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 const randomImageHolder = document.querySelector('.random-image-container');
+const savedImagesContainer =document.querySelector('.savedImages');
+let refreshImage = document.querySelector('#refresh');
+const selectButton = document.querySelector('#select');
+let emailAddr = document.querySelector('#email');
+// let imageOnScreen = " "; // temp images link
+let imagesStored = []; //person saved images and email to array
+
 
 // ------------------------------------------
-//  RANDOM PAGE NUMBER
+//  GENERATE  RANDOM PAGE NUMBER WHEN PAGE LOADS
 // ------------------------------------------
 
-const randomPage = Math.floor(Math.random() * 10) + 1;
+let randomPage = Math.floor(Math.random() * 10) + 1;
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://picsum.photos/v2/list?page=' + `${randomPage}`+ '&limit=100')
-  // fetch('https://picsum.photos/list')
-  .then(response => response.json())
-  .then(data => console.log(data[0].download_url))
-  .then(data => console.log(data))
+
+
+function fetchData(url) {
+
+  return fetch(url)
+            .then(res => res.json())
+
+}
+
+//getting img url
+fetchData('https://picsum.photos/v2/list?page=' + `${randomPage}`+ '&limit=100')
   .then(data => generateImage(data[0].download_url))
+
 
 
 
@@ -23,6 +37,7 @@ fetch('https://picsum.photos/v2/list?page=' + `${randomPage}`+ '&limit=100')
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
+// create img container
 function generateImage(data) {
   const html = `
     <img src='${data}' alt>
@@ -31,15 +46,39 @@ function generateImage(data) {
 }
 
 
+//regenerating new image and storing link
+function fetchRandomImage(){
+  const img = randomImageHolder.querySelector('img');
+  const randomPage = Math.floor(Math.random() * 10) + 1;
+  fetchData(`https://picsum.photos/v2/list?page=${randomPage}&limit=100`)
+    .then(data => {
+         img.src = data[0].download_url;
+    })
+    // .then(imageOnScreen = img.src);
 
+}
 
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-
-
+//call function to regenerate and display new image
+refreshImage.addEventListener('click', fetchRandomImage);
 
 // ------------------------------------------
-//  POST DATA
+//STORE SELECTED IMAGES
 // ------------------------------------------
+
+//function to check button select clicked and input email field not empty if not empty store email and img url
+selectButton.addEventListener('click', function(){
+  if (emailAddr.value.length >= 1) {
+    const img = randomImageHolder.querySelector('img');
+    console.log('not empty');
+    imagesStored.push(randomImageHolder.querySelector('img').src);
+  }else {
+    console.log("empty");
+
+  }
+});
+//
+// console.log(emailAddr.value);
